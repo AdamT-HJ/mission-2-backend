ed702d2f9ac981370254d619a136e0f571afe95b
potato <Brittanycc@missionreadyhq.com>
2025-05-24 19:39:57 +1200

    Commented current code for API1 in server.js

--- server.js
+++ server.js
@@ -32,16 +32,23 @@ app.get("/", (req, res) => {
 
 //----------BRITT (lines 136-236)-------//
 function carValue(model, year) {
+  //Establishing the car value based on the model and year of the vehicle
   const carAlphabet = Object.fromEntries(
     Array.from({ length: 26 }, (_, i) => [String.fromCharCode(97 + i), i + 1])
-  );
+  ); //Array is created of 26 elements and then maps
+  //_ acts like a placeholder and i is the index
+  // 97 is the ASCII code for 'a'
+  // string.fromCharCode converts a number into its corresponding character
 
   const modelValue = model
     .toLowerCase()
     .split("")
-    .reduce((sum, char) => +(carAlphabet[char] || 0), 0);
+    .reduce((sum, char) => sum + (carAlphabet[char] || 0), 0);
+    // Reduces the function | sum/reduce starts at 0 and char is each character from the array, adds it to the sum, + the car alphabet 
+    // and if there is no char found in car alphabet it uses 0.
 
   return modelValue * 100 + parseInt(year);
+  // Final carValue addition modelValue(60) * 100 + Year(2015) = carValue
 }
 
 //-------ADAM (lines 237-337)------//
